dca6176ff3325bf5adce26de9cded02d533d2ae5
tvnguyen <vituongnnguyen@gmail.com>
2023-05-22 12:04:36 +0700

    Appy change

--- main.tf
+++ main.tf
@@ -9,3 +9,19 @@ terraform {
 provider "aws" {
     region="us-east-1"
 }
+
+resource "aws_instance" "terraform_instance" {
+  ami = "ami-02396cdd13e9a1257"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Test_terraform_tvnguyen"
+  }
+}
+
+resource "aws_ebs_volume" "terraform_ebs" {
+  availability_zone = "us-east-1d"
+  size = 2
+  tags = {
+    Name = "Test_terraform_tvnguyen"
+  }
+}
